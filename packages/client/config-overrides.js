2f4d3c2a91a90f2d049b75e3be5686e2ed836bc8
Kiiiyo <kiiiyo.jp@gmail.com>
2022-03-03 14:10:27 +0900

    :wrench: add swc/jest setup

---
+++ packages/client/config-overrides.js
@@ -0,0 +1,18 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const {
+  override,
+  addWebpackResolve
+  // addBabelPresets,
+  // addBabelPlugins
+} = require('customize-cra')
+const path = require('path')
+
+module.exports = override(
+  // addBabelPresets(['@babel/preset-react']),
+  // addBabelPlugins(['@emotion/babel-plugin']),
+  addWebpackResolve({
+    alias: {
+      '@': path.resolve(__dirname, './src/')
+    }
+  })
+)
---
+++ packages/client/setup.ts
@@ -0,0 +1,5 @@
+import '@testing-library/jest-dom'
+
+process.env = {
+  ...process.env
+}
