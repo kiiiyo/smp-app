cedf96626dcbb46aa992dada1c57b23a369fd8cf
Kiiiyo <kiiiyo.jp@gmail.com>
2022-03-03 11:33:49 +0900

    :wrench: modify lint config

--- packages/client/.eslintrc.js
+++ packages/client/.eslintrc.js
@@ -2,21 +2,41 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    node: true,
+    node: true
   },
   extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended",
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier'
   ],
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
-      jsx: true,
+      jsx: true
     },
-    ecmaVersion: "latest",
-    sourceType: "module",
+    ecmaVersion: 'latest',
+    sourceType: 'module'
   },
-  plugins: ["react", "@typescript-eslint"],
-  rules: {},
-};
+  settings: {
+    'import/resolver': {
+      typescript: {
+        project: './tsconfig.json'
+      }
+    }
+  },
+  plugins: ['react', '@typescript-eslint', 'react-hooks'],
+  rules: {
+    // Enable
+    quotes: ['warn', 'single'],
+    'no-extra-semi': 'warn',
+    '@typescript-eslint/no-unused-vars': ['error'],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+
+    // Disable
+    'no-unused-vars': 'off',
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/no-empty-function': 'off'
+  }
+}
