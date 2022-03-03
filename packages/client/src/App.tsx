b87ddfa2adfa257d22f95cbb907e507b8946aead
Kiiiyo <kiiiyo.jp@gmail.com>
2022-03-03 14:36:25 +0900

    :recycle: modify refactoring code

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,11 +1,7 @@
-import logo from './logo.svg'
-import Button from '@mui/material/Button'
-
 function App() {
   return (
     <div>
-      <Button variant="contained">Hello World</Button>;
-      <img src={logo} className="App-logo" alt="logo" />
+      <h1>Social media Marketing Platform</h1>
     </div>
   )
 }
